5487efe128557ce0a0376781354cd5042264dca9
SHAIKH SHAWON AREFIN SHIMON <shimon0505004@gmail.com>
2022-09-25 23:37:24 -0400

    Initial commits for completing A1 Part 2

---
+++ A1/RL.py
@@ -0,0 +1,61 @@
+import numpy as np
+import MDP
+
+class RL:
+    def __init__(self,mdp,sampleReward):
+        '''Constructor for the RL class
+
+        Inputs:
+        mdp -- Markov decision process (T, R, discount)
+        sampleReward -- Function to sample rewards (e.g., bernoulli, Gaussian).
+        This function takes one argument: the mean of the distributon and 
+        returns a sample from the distribution.
+        '''
+
+        self.mdp = mdp
+        self.sampleReward = sampleReward
+
+    def sampleRewardAndNextState(self,state,action):
+        '''Procedure to sample a reward and the next state
+        reward ~ Pr(r)
+        nextState ~ Pr(s'|s,a)
+
+        Inputs:
+        state -- current state
+        action -- action to be executed
+
+        Outputs: 
+        reward -- sampled reward
+        nextState -- sampled next state
+        '''
+
+        reward = self.sampleReward(self.mdp.R[action,state])
+        cumProb = np.cumsum(self.mdp.T[action,state,:])
+        nextState = np.where(cumProb >= np.random.rand(1))[0][0]
+        return [reward,nextState]
+
+    def qLearning(self,s0,initialQ,nEpisodes,nSteps,epsilon=0,temperature=0):
+        '''qLearning algorithm.  
+        When epsilon > 0: perform epsilon exploration (i.e., with probability epsilon, select action at random )
+        When epsilon == 0 and temperature > 0: perform Boltzmann exploration with temperature parameter
+        When epsilon == 0 and temperature == 0: no exploration (i.e., selection action with best Q-value)
+
+        Inputs:
+        s0 -- initial state
+        initialQ -- initial Q function (|A|x|S| array)
+        nEpisodes -- # of episodes (one episode consists of a trajectory of nSteps that starts in s0
+        nSteps -- # of steps per episode
+        epsilon -- probability with which an action is chosen at random
+        temperature -- parameter that regulates Boltzmann exploration
+
+        Outputs: 
+        Q -- final Q function (|A|x|S| array)
+        policy -- final policy
+        '''
+
+        # temporary values to ensure that the code compiles until this
+        # function is coded
+        Q = np.zeros([self.mdp.nActions,self.mdp.nStates])
+        policy = np.zeros(self.mdp.nStates,int)
+
+        return [Q,policy]    
---
+++ A1/TestRL.py
@@ -0,0 +1,17 @@
+import numpy as np
+import MDP
+import RL
+
+
+''' Construct simple MDP as described in Lecture 1b Slides 17-18'''
+T = np.array([[[0.5,0.5,0,0],[0,1,0,0],[0.5,0.5,0,0],[0,1,0,0]],[[1,0,0,0],[0.5,0,0,0.5],[0.5,0,0.5,0],[0,0,0.5,0.5]]])
+R = np.array([[0,0,10,10],[0,0,10,10]])
+discount = 0.9        
+mdp = MDP.MDP(T,R,discount)
+rlProblem = RL.RL(mdp,np.random.normal)
+
+# Test Q-learning 
+[Q,policy] = rlProblem.qLearning(s0=0,initialQ=np.zeros([mdp.nActions,mdp.nStates]),nEpisodes=1000,nSteps=100,epsilon=0.3)
+print("\nQ-learning results")
+print(Q)
+print(policy)
