701e41786a006c299aa0ff1ae8ed2b6ee0b375a5
SHAIKH SHAWON AREFIN SHIMON <shimon0505004@gmail.com>
2022-10-13 10:55:40 -0400

    Most code done for A2

--- A2/RL2.py
+++ A2/RL2.py
@@ -1,8 +1,9 @@
 import numpy as np
 import MDP
 
+
 class RL2:
-    def __init__(self,mdp,sampleReward):
+    def __init__(self, mdp, sampleReward):
         '''Constructor for the RL class
 
         Inputs:
@@ -15,7 +16,7 @@ class RL2:
         self.mdp = mdp
         self.sampleReward = sampleReward
 
-    def sampleRewardAndNextState(self,state,action):
+    def sampleRewardAndNextState(self, state, action):
         '''Procedure to sample a reward and the next state
         reward ~ Pr(r)
         nextState ~ Pr(s'|s,a)
@@ -29,12 +30,12 @@ class RL2:
         nextState -- sampled next state
         '''
 
-        reward = self.sampleReward(self.mdp.R[action,state])
-        cumProb = np.cumsum(self.mdp.T[action,state,:])
+        reward = self.sampleReward(self.mdp.R[action, state])
+        cumProb = np.cumsum(self.mdp.T[action, state, :])
         nextState = np.where(cumProb >= np.random.rand(1))[0][0]
-        return [reward,nextState]
+        return [reward, nextState]
 
-    def epsilonGreedyBandit(self,nIterations):
+    def epsilonGreedyBandit(self, nIterations):
         '''Epsilon greedy algorithm for bandits (assume no discount factor).  Use epsilon = 1 / # of iterations.
 
         Inputs:
@@ -47,10 +48,35 @@ class RL2:
         # temporary values to ensure that the code compiles until this
         # function is coded
         empiricalMeans = np.zeros(self.mdp.nActions)
+        state = 0
+        n_a = np.zeros(self.mdp.nActions)  # Counts current number of times action a is selected.
 
-        return empiricalMeans
+        rewards_earned = np.zeros(nIterations + 1)
+        V = rewards_earned[0]   #Total Reward
 
-    def thompsonSamplingBandit(self,prior,nIterations,k=1):
+        for iteration in range(1, (nIterations + 1)):
+            epsilon = (1 / iteration)
+            p = np.random.random(1)
+            if p < epsilon:
+                action = np.random.randint(self.mdp.nActions)
+            else:
+                action = np.argmax(empiricalMeans)
+
+            [reward, nextState] = self.sampleRewardAndNextState(state, action)
+
+            # Updating empirical mean for selected action with new reward
+            previous_total_reward_for_action = (n_a[action] * empiricalMeans[action])
+            updated_total_reward_for_action = (previous_total_reward_for_action + reward)
+            n_a[action] = n_a[action] + 1  # updating number of times the selected action was encountered.
+            new_empirical_mean_for_action = updated_total_reward_for_action / n_a[action]
+            empiricalMeans[action] = new_empirical_mean_for_action
+            # print("Updated Empirical Means: " + str(empiricalMeans))
+            rewards_earned[iteration] = reward          #Keeping track of reward earned at iteration
+            V += rewards_earned[iteration]
+
+        return empiricalMeans, rewards_earned, V
+
+    def thompsonSamplingBandit(self, prior, nIterations, k=1):
         '''Thompson sampling algorithm for Bernoulli bandits (assume no discount factor)
 
         Inputs:
@@ -65,10 +91,43 @@ class RL2:
         # temporary values to ensure that the code compiles until this
         # function is coded
         empiricalMeans = np.zeros(self.mdp.nActions)
+        state = 0
+        rewards_earned = np.zeros(nIterations + 1)
+        n_a = np.zeros(self.mdp.nActions)  # Counts current number of times action a is selected.
+        V = rewards_earned[0]  # Total Reward
 
-        return empiricalMeans
+        for iteration in range(1, nIterations + 1):
+            R_hat = np.zeros(self.mdp.nActions)
+            for a in range(self.mdp.nActions):
+                k_sample_rewards_for_action_a = np.random.beta(a=prior[a, 0],
+                                                               # Alpha hyperparameter for beta distribution for arm a
+                                                               b=prior[a, 1],
+                                                               # Beta hyperparameter for beta distribution for arm a
+                                                               size=k  # number of sampled average rewards
+                                                               )  # Sample k rewards from beta distribution for action a.
 
-    def UCBbandit(self,nIterations):
+                R_hat[a] = np.mean(k_sample_rewards_for_action_a)  # Estimate empirical average of K sample
+                # rewards for action a
+
+            action = np.argmax(R_hat)  #choose the best action
+            [reward, nextState] = self.sampleRewardAndNextState(state, action)  #Select the reward for the best action
+
+            # Updating empirical mean for selected action with new reward
+            previous_total_reward_for_action = (n_a[action] * empiricalMeans[action])
+            updated_total_reward_for_action = (previous_total_reward_for_action + reward)
+            n_a[action] = n_a[action] + 1  # updating number of times the selected action was encountered.
+            new_empirical_mean_for_action = updated_total_reward_for_action / n_a[action]
+            empiricalMeans[action] = new_empirical_mean_for_action
+
+            rewards_earned[iteration] = reward
+            V += rewards_earned[iteration]
+
+            prior[action, reward] += 1     # Reward always between 0 and 1, we update the prior of the best
+            # action based on the reward
+
+        return empiricalMeans, rewards_earned, V
+
+    def UCBbandit(self, nIterations):
         '''Upper confidence bound algorithm for bandits (assume no discount factor)
 
         Inputs:
@@ -81,5 +140,28 @@ class RL2:
         # temporary values to ensure that the code compiles until this
         # function is coded
         empiricalMeans = np.zeros(self.mdp.nActions)
+        state = 0
+        n_a = np.zeros(self.mdp.nActions)  # Counts current number of times action a is selected.
 
-        return empiricalMeans
+        rewards_earned = np.zeros(nIterations + 1)
+        V = rewards_earned[0]  # Total Reward
+
+        epsilon = (10 ** -8)  # To avoid overflow issues.
+
+        for iteration in range(1, nIterations + 1):
+            n = iteration#(iteration-1)+epsilon
+            action = np.argmax((empiricalMeans + (np.sqrt(2 * np.log(n) / (n_a + epsilon)))))
+
+            [reward, nextState] = self.sampleRewardAndNextState(state, action)
+
+            # Updating empirical mean for selected action with new reward
+            previous_total_reward_for_action = (n_a[action] * empiricalMeans[action])
+            updated_total_reward_for_action = (previous_total_reward_for_action + reward)
+            n_a[action] = n_a[action] + 1  # updating number of times the selected action was encountered.
+            new_empirical_mean_for_action = updated_total_reward_for_action / n_a[action]
+            empiricalMeans[action] = new_empirical_mean_for_action
+            # print("Updated Empirical Means: " + str(empiricalMeans))
+            rewards_earned[iteration] = reward
+            V += rewards_earned[iteration]
+
+        return empiricalMeans, rewards_earned, V
